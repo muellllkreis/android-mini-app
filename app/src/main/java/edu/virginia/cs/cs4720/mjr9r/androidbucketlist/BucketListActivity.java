09ca8f0cc882db17ebafb2e317974d6ffec30288
Cabell Glancy <bcg7mf@virginia.edu>
2017-09-04 16:11:10 -0400

    Implemented explicit intent for edit in main activity

--- app/src/main/java/edu/virginia/cs/cs4720/mjr9r/androidbucketlist/BucketListActivity.java
+++ app/src/main/java/edu/virginia/cs/cs4720/mjr9r/androidbucketlist/BucketListActivity.java
@@ -10,7 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 import android.widget.CheckBox;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,13 +30,6 @@ public class BucketListActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         RecyclerView rvItems = (RecyclerView) findViewById(R.id.rvItems);
 
         bucketlist = BucketItem.createBucketList(20);
@@ -63,4 +58,11 @@ public class BucketListActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void edit_item(View view) {
+        Intent edit = new Intent(this,Edit_Item.class);
+        this.startActivity(edit);
+    }
+
+
 }
--- app/src/main/java/edu/virginia/cs/cs4720/mjr9r/androidbucketlist/Edit_Item.java
+++ app/src/main/java/edu/virginia/cs/cs4720/mjr9r/androidbucketlist/Edit_Item.java
@@ -3,6 +3,8 @@ package edu.virginia.cs.cs4720.mjr9r.androidbucketlist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/* Created by Cabell-Glancy on 04/09/17 */
+
 public class Edit_Item extends AppCompatActivity {
 
     @Override
